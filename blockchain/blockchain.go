c6a2599311b9cd238b0f80ff1a8d77ca97b97dc5
Xingyu-Jiang <xzj5086@psu.edu>
2024-02-16 23:25:52 -0500

    Using Encrypt and Decrypt
    Using Encrypt and Decrypt in the crypto file that is proved to encrypt data in the block chain.

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,6 +1,8 @@
 package blockchain
 
 import (
+	"CMPSC488SP24SecThursday/crypto"
+	"bytes"
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
@@ -9,11 +11,17 @@ import (
 	"time"
 )
 
+// EncryptedData represents the encrypted data in a block.
+type EncryptedData struct {
+	Ciphertext []byte
+	Error      error
+}
+
 // Block represents a single block in the blockchain.
 type Block struct {
 	Index     int
 	Timestamp string
-	Data      string
+	Data      EncryptedData
 	PrevHash  string
 	Hash      string
 }
@@ -24,23 +32,12 @@ type Blockchain struct {
 	Chain      []Block
 }
 
-// EncryptData encrypts the data in a block.
-//TODO: Implement function
-/*
-Purpose: To Encrypt Data for Block Creation
-	data: string - data to be encrypted
-	returns: string
-*/
-func EncryptData(data string) string {
-	return data
-}
-
 // CalculateHash calculates the SHA-256 hash of a block. TODO: Refactor?
 /*
 Purpose: Calculates the hash of a block with sha256.
 uses the fields of Block to generate hash of a defined difficulty
-	block : Block - input block
-	nonce : int - takes input nonce to include nonce in encoded string
+	block: Block - input block
+	nonce: int - takes input nonce to include nonce in encoded string
 	returns: string
 */
 
@@ -59,8 +56,8 @@ func CalculateHash(block Block, nonce int) string {
 // ProofOfWork Proof of Work Algorithm Base
 /*
 Purpose: Helper function//consensus and validation algorithm to ensure a valid hash is generated.
-	block : Block - input block
-	difficulty : int - difficulty requirement of hash
+	block: Block - input block
+	difficulty: int - difficulty requirement of hash
 	returns: string
 */
 func ProofOfWork(block Block, difficulty int) string {
@@ -85,9 +82,9 @@ func ProofOfWork(block Block, difficulty int) string {
 // VerifyBlock verifies the hash of a block.
 /*
 Purpose: Verifies given Block data has not been tampered.
-uses the fields of Block to generate hash and verifies that both hashes match.
-compares both previous block's hash with this block to ensure that they match
-verifies block by mining given block and checking previous block in blockchain
+uses the fields of Block to generate hash and verify that both hashes match.
+compare both previous block's hash with this block to ensure that they match
+verifies block by mining given block and checking the previous block in blockchain
 ***
 	--NOTE: The block must not be in the blockchain
 	This Function should be called before the block is added to the Blockchain--
@@ -140,12 +137,16 @@ inserts the block into the blockchain with AddToBlockchain()
 */
 func (bc *Blockchain) CreateBlock(data string) {
 	prevBlock := bc.Chain[len(bc.Chain)-1]
+	ciphertext, err := crypto.Encrypt([]byte(data), []byte("1234567890123456"))
 	newBlock := Block{
 		Index:     prevBlock.Index + 1,
 		Timestamp: time.Now().String(),
-		Data:      EncryptData(data), // encrypt the data before storing it
-		PrevHash:  prevBlock.Hash,
-		Hash:      "",
+		Data: EncryptedData{
+			Ciphertext: ciphertext,
+			Error:      err,
+		}, // encrypt the data before storing it
+		PrevHash: prevBlock.Hash,
+		Hash:     "",
 	}
 	newBlock.Hash = ProofOfWork(newBlock, bc.Difficulty)
 	bc.AddToBlockchain(newBlock)
@@ -154,7 +155,7 @@ func (bc *Blockchain) CreateBlock(data string) {
 // AddToBlockchain adds a new block into the blockchain
 /*
 Purpose: Adds a given block into the blockchain
-	block : Block - input block to be inserted
+	block: Block - input block to be inserted
 */
 func (bc *Blockchain) AddToBlockchain(block Block) {
 	bc.Chain = append(bc.Chain, block)
@@ -163,34 +164,30 @@ func (bc *Blockchain) AddToBlockchain(block Block) {
 // NewBlockchain creates a new blockchain with a genesis block.
 /*
 Purpose: Creates a new Blockchain and initializes it with a genesis block.
-	difficulty : determines the difficulty requirement of the hash
+	difficulty: determines the difficulty requirement of the hash
 	returns: Blockchain
 */
 func NewBlockchain(difficulty int) *Blockchain {
+	ciphertext, err := crypto.Encrypt([]byte("Genesis Block"), []byte("1234567890123456"))
 	genesisBlock := Block{
 		Index:     0,
 		Timestamp: time.Now().String(),
-		Data:      "Genesis Block",
-		PrevHash:  "",
-		Hash:      "",
+		Data: EncryptedData{
+			Ciphertext: ciphertext,
+			Error:      err,
+		},
+		PrevHash: "",
+		Hash:     "",
 	}
 	genesisBlock.Hash = ProofOfWork(genesisBlock, difficulty)
 	return &Blockchain{Chain: []Block{genesisBlock}, Difficulty: difficulty}
 }
 
-// DecryptData decrypts the data in a block.
-//TODO: Implement function
-/*
-Purpose: To Decrypt Data for Block Processing
-	data: string - data to be decrypted
-	returns: string
-*/
-func DecryptData(data string) string { return data }
-
 // SerializeBlock Converting the block struct to JSON
 /*
 Purpose: Serialize blocks that are created.
-	returns: JSON file of the block
+	block: block to serialize
+	Returns: serialized data of the block
 */
 func SerializeBlock(block Block) ([]byte, error) {
 	return json.Marshal(block)
@@ -199,10 +196,37 @@ func SerializeBlock(block Block) ([]byte, error) {
 // DeserializeBlock Converting the JSON string back to a block struct.
 /*
 Purpose: Deserialize blocks from a JSON file.
-	returns: Block and Error value
+	data: data to Deserialize
+	Returns: Block and Error value
 */
 func DeserializeBlock(data []byte) (Block, error) {
 	var block Block
 	err := json.Unmarshal(data, &block)
 	return block, err
+}
+
+// EqualBlocks Check if two blocks are equal
+/*
+Purpose: To check if all the fields of the two given blocks are the same
+	block1, block2: The two block to compare
+	Return: bool
+*/
+func EqualBlocks(block1, block2 Block) bool {
+	if block1.Index != block2.Index {
+		return false
+	}
+	if block1.Timestamp != block2.Timestamp {
+		return false
+	}
+	// Compare the Ciphertext field of EncryptedData using bytes.Equal()
+	if !bytes.Equal(block1.Data.Ciphertext, block2.Data.Ciphertext) {
+		return false
+	}
+	if block1.PrevHash != block2.PrevHash {
+		return false
+	}
+	if block1.Hash != block2.Hash {
+		return false
+	}
+	return true
 }
