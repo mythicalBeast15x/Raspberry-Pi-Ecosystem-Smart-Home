0f2d568e5ebdeaf2f3d06db25dee061e4d34e95a
karenkaur29 <145386446+karenkaur29@users.noreply.github.com>
2024-03-16 17:50:37 -0400

    Update cofigLoader.go

--- CofigStruct/cofigLoader.go
+++ CofigStruct/cofigLoader.go
@@ -1,4 +1,30 @@
-package configstruct
+/*package configstruct
+
+import (
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+)
+
+func main() {
+    configFile := "config.json"
+    fileContent, err := ioutil.ReadFile(configFile)
+    if err != nil {
+        log.Fatalf("Error reading configuration file: %s", err)
+    }
+
+    var config ConfigurationStructure
+    if err := json.Unmarshal(fileContent, &config); err != nil {
+        log.Fatalf("Error decoding configuration: %s", err)
+    }
+
+*/
+
+
+
+
+
 
 /*
 func main() {
