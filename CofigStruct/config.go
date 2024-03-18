35f8faab01f8ca1316e764b97074095b8241e25c
karenkaur29 <145386446+karenkaur29@users.noreply.github.com>
2024-03-17 20:31:54 -0400

    Update config.go

--- CofigStruct/config.go
+++ CofigStruct/config.go
@@ -6,20 +6,24 @@ type Appliance struct {
 	Status      string `json:"status"`
 }
 
+// SetStatus updates the status of the appliance.
 func (a *Appliance) SetStatus(newStatus string) {
 	a.Status = newStatus
 }
 
+// GetStatus returns the current status of the appliance.
 func (a *Appliance) GetStatus() string {
 	return a.Status
 }
 
+// Device that can control one or more appliances -> connected appliances
 type Device struct {
-	DeviceID              string   `json:"deviceID"`
-	ConnectedApplianceIDs []string `json:"connectedAppliances"`
+	DeviceID              string   `json:"deviceID"` // Identifying device 
+	ConnectedApplianceIDs []string `json:"connectedAppliances"` // ConnectedApplianceIDs saves the IDs of appliances connected to each device.
 	ConnectedAppliances   []*Appliance
 }
 
+// ConfigurationStructure holds the configuration needed for the application to work with devices and appliances.
 type ConfigurationStructure struct {
 	PrivateKey string       `json:"privateKey"`
 	PublicKey  string       `json:"publicKey"`
